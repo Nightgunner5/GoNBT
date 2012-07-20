bcaa0557139f1e16f0f37bc97f09ebb69881d4a1
BeMasher <bemasher@gmail.com>
2012-07-20 04:41:43 -0600

    Added comments.

--- example.go
+++ example.go
@@ -12,11 +12,17 @@ const (
 	NBTFILE = "bigtest.nbt"
 )
 
+// The main type to be decoded must have the same name as the
+// first non-blank compound tag name which in the case of bigtest.nbt is Level
 type Level struct {
+	// Nested type structures are analogous to compound tags
 	Nested Nested `nbt:"nested compound test"`
 	ByteTest byte
 	IntTest int32
 	StringTest string
+	// Struct field tags behave much in the same way as the json
+	// or xml encoding packages in golang, the name of the field
+	// may be specified if the field name cannot be represented legally in go
 	ListTestLong []int64 `nbt:"listTest (long)"`
 	DoubleTest float64
 	FloatTest float32
@@ -54,8 +60,8 @@ func main() {
 	var l Level
 	nbt.Read(nbtFile, &l)
 	
+	// Using json for pretty indented printing of the structure
 	data, err := json.MarshalIndent(l, "", "\t")
 	errhandler.Handle("Error marshalling json: ", err)
-	
 	fmt.Printf("%s\n", data)
 }
