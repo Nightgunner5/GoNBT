0a61b6e73aab6490cfca3f6732591f217ad01703
Ben L <nightgunner5@llamaslayers.net>
2012-07-23 19:10:55 -0500

    go fmt

--- nbt.go
+++ nbt.go
@@ -1,12 +1,12 @@
 package nbt
 
 import (
+	"encoding/binary"
+	"github.com/bemasher/errhandler"
 	"io"
 	"math"
 	"reflect"
 	"strings"
-	"encoding/binary"
-	"github.com/bemasher/errhandler"
 )
 
 // Define constants defining all of the possible tag types
@@ -32,18 +32,30 @@ type TagType byte
 // Provides a string representation of the tag type.
 func (t TagType) String() string {
 	switch t {
-		case TAG_END: return "TAG_END"
-		case TAG_BYTE: return "TAG_BYTE"
-		case TAG_SHORT: return "TAG_SHORT"
-		case TAG_INT: return "TAG_INT"
-		case TAG_LONG: return "TAG_LONG"
-		case TAG_FLOAT: return "TAG_FLOAT"
-		case TAG_DOUBLE: return "TAG_DOUBLE"
-		case TAG_BYTE_ARRAY: return "TAG_BYTE_ARRAY"
-		case TAG_STRING: return "TAG_STRING"
-		case TAG_LIST: return "TAG_LIST"
-		case TAG_COMPOUND: return "TAG_COMPOUND"
-		case TAG_INT_ARRAY: return "TAG_INT_ARRAY"
+	case TAG_END:
+		return "TAG_END"
+	case TAG_BYTE:
+		return "TAG_BYTE"
+	case TAG_SHORT:
+		return "TAG_SHORT"
+	case TAG_INT:
+		return "TAG_INT"
+	case TAG_LONG:
+		return "TAG_LONG"
+	case TAG_FLOAT:
+		return "TAG_FLOAT"
+	case TAG_DOUBLE:
+		return "TAG_DOUBLE"
+	case TAG_BYTE_ARRAY:
+		return "TAG_BYTE_ARRAY"
+	case TAG_STRING:
+		return "TAG_STRING"
+	case TAG_LIST:
+		return "TAG_LIST"
+	case TAG_COMPOUND:
+		return "TAG_COMPOUND"
+	case TAG_INT_ARRAY:
+		return "TAG_INT_ARRAY"
 	}
 	return "TAG_UNKNOWN"
 }
@@ -53,10 +65,10 @@ func StructFields(d reflect.Value) map[string]reflect.Value {
 	// Make sure this is a struct
 	if d.Kind() == reflect.Struct {
 		tags := make(map[string]reflect.Value, 0)
-		
+
 		// Include the type's name as well, this is used when decoding compound tags
 		tags[strings.ToLower(d.Type().Name())] = d
-		
+
 		// For each of the fields in the struct
 		for i := 0; i < d.NumField(); i++ {
 			// If the nbt key is present in the field's tags use that instead otherwise just use the field name
@@ -93,39 +105,50 @@ func readTag(r io.Reader, tagType TagType, data reflect.Value) (name string, new
 	} else {
 		newTagType = TagType(tagType)
 	}
-	
+
 	// Get a list of the fields for the given struct
 	var field reflect.Value
 	fields := StructFields(data)
-	
+
 	// If this field is valid it isn't a zero value so assign it's field from the struct map
 	if data.IsValid() {
 		field = fields[strings.ToLower(name)]
 	}
-	
+
 	// If this is the root tag keep going anyway and just decode into the current value
 	if name == "" {
 		field = data
 	}
-	
+
 	var (
 		temp interface{}
 		list reflect.Value
 	)
 	switch newTagType {
-		case TAG_BYTE: temp = ReadByte(r)
-		case TAG_SHORT: temp = ReadShort(r)
-		case TAG_INT: temp = ReadInt(r)
-		case TAG_LONG: temp = ReadLong(r)
-		case TAG_FLOAT: temp = ReadFloat(r)
-		case TAG_DOUBLE: temp = ReadDouble(r)
-		case TAG_BYTE_ARRAY: temp = ReadByteArray(r)
-		case TAG_STRING: temp = ReadString(r)
-		case TAG_INT_ARRAY: temp = ReadIntArray(r)
-		case TAG_COMPOUND: ReadCompound(r, field)
-		case TAG_LIST: list = ReadList(r, field)
+	case TAG_BYTE:
+		temp = ReadByte(r)
+	case TAG_SHORT:
+		temp = ReadShort(r)
+	case TAG_INT:
+		temp = ReadInt(r)
+	case TAG_LONG:
+		temp = ReadLong(r)
+	case TAG_FLOAT:
+		temp = ReadFloat(r)
+	case TAG_DOUBLE:
+		temp = ReadDouble(r)
+	case TAG_BYTE_ARRAY:
+		temp = ReadByteArray(r)
+	case TAG_STRING:
+		temp = ReadString(r)
+	case TAG_INT_ARRAY:
+		temp = ReadIntArray(r)
+	case TAG_COMPOUND:
+		ReadCompound(r, field)
+	case TAG_LIST:
+		list = ReadList(r, field)
 	}
-	
+
 	// If field was invalid we still needed to decode the tag, if we got here and it's valid we need to store it
 	if field.IsValid() {
 		// If temp has a value in it, then it wasn't a list or compound tag so go ahead and store it
@@ -134,7 +157,7 @@ func readTag(r io.Reader, tagType TagType, data reflect.Value) (name string, new
 			if field.Type().AssignableTo(v.Type()) {
 				field.Set(v)
 			}
-		// If the list valud is valid, make sure we store it
+			// If the list valud is valid, make sure we store it
 		} else if list.IsValid() {
 			field.Set(list)
 		}
@@ -217,7 +240,8 @@ func ReadIntArray(r io.Reader) (list []int32) {
 
 // Reads a list of tags into the given data structure until a tag of TAG_END is encountered.
 func ReadCompound(r io.Reader, data reflect.Value) {
-	for _, tagType := readTag(r, TAG_UNKNOWN, data); tagType != TAG_END; _, tagType = readTag(r, TAG_UNKNOWN, data) {}
+	for _, tagType := readTag(r, TAG_UNKNOWN, data); tagType != TAG_END; _, tagType = readTag(r, TAG_UNKNOWN, data) {
+	}
 }
 
 // Reads a list of tags into a list, the elements of the list don't have names or types specified, so readTag requires the tag type to be given.
